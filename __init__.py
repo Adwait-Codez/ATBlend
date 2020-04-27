e9d498f0b875f5c437ffc633a8a5319c6afa4f89
adwait <adwait.34@gmail.com>
2020-04-27 14:12:17 +0530

    Added project Files

---
+++ __init__.py
@@ -0,0 +1,19 @@
+bl_info = {
+    "name" : "ATblend",
+    "author" : "Adwait.R.K",
+    "description" : "Adwait 2D 's Blender addon for 2D animation",
+    "blender" : (2,80,0),
+    "location" : "View3D",
+    "warning" : "",
+    "category" : "Generic"
+    }
+
+import bpy
+
+from . Centcur import Test_OT_Operator
+from . test_panel import Test_PT_Panel
+
+
+classes = (Test_OT_Operator, Test_PT_Panel, HOK_OT_Operator)
+
+register, unregister = bpy.utils.register_classes_factory(classes)
